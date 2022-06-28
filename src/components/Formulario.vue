ddd163e3400054f725c1b8801dcfde1294cceabf
julio cesar aguero <julio.aguero.gomez@gmail.com>
2022-06-28 18:30:39 -0500

    formulario prospecto telefono opcional completo

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -8,45 +8,20 @@
             <h2>COTIZA TU PROYECTO CON NOSOTROS:</h2>
             <hr />
             <label for="nombre" class="form-label">
-              <i class="fas fa-user"></i> Nombre:</label
-            >
-            <input
-              type="text"
-              name="nombre"
-              id="nombre"
-              placeholder="Ingresa tu nombre*"
-              v-model.trim="formData.name"
-              required
-            />
+              <i class="fas fa-user"></i> Nombre:</label>
+            <input type="text" name="nombre" id="nombre" placeholder="Ingresa tu nombre*" v-model.trim="formData.name"
+              required />
             <label for="correo" class="form-label">
-              <i class="fas fa-envelope"></i> Correo:</label
-            >
-            <input
-              v-model.trim="formData.email"
-              type="email"
-              name="correo"
-              id="correo"
-              placeholder="Correo Corporativo*"
-              required
-            />
+              <i class="fas fa-envelope"></i> Correo:</label>
+            <input v-model.trim="formData.email" type="email" name="correo" id="correo"
+              placeholder="Correo Corporativo*" required />
 
-            <label for="nombre" class="form-label">
-              <i class="fab fa-whatsapp"></i> Teléfono:</label
-            >
-            <input
-              v-model.trim="formData.telefono"
-              type="text"
-              name="nombre"
-              id="telefono"
-              placeholder="Ingresa tu teléfono de contacto"
-            />
+            <label for="telefono" class="form-label">
+              <i class="fab fa-whatsapp"></i> Teléfono:</label>
+            <input v-model.trim="formData.telefono" type="text" name="telefono" id="telefono"
+              placeholder="Ingresa tu teléfono de contacto" />
             <label for="categoria">¿Qué te interesa?</label>
-            <select
-              class="select"
-              name="categoria"
-              id="categoria"
-              v-model="selected"
-            >
+            <select class="select" name="categoria" id="categoria" v-model="selected">
               <option value="Regalos dia de las madres">
                 Regalos corporativos día de las madres
               </option>
@@ -104,20 +79,34 @@ export default {
       let expnumber =
         /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
 
-      if(data.nombre ==""){
-      this.showMensaje = true;
-      this.mensaje = "Ingresa un nombre";
+//ingreso nombre ?
+      if (data.nombre == "") {
+        this.showMensaje = true; 
+        this.mensaje = "Ingresa un nombre";
       }
+
+// tiene un e-mail corporativo valido 
       if (expdominio.test(this.formData.email)) {
         this.showMensaje = true;
         this.mensaje = "Ingresa un correo corporativo";
-      } else {
+      } 
+      // o de lo contrario 
+        else {
+          //si no cumple con el formato se muestra mensaje
         if (!exp.test(this.formData.email)) {
           this.showMensaje = true;
           this.mensaje = "Ingresa un correo valido";
-        } else {
+        } 
+        // de lo contrario continua
+        else {
           this.showMensaje = true;
-          if (!expnumber.test(this.formData.telefono)) {
+          // si el numero ingresado cumple el formato
+          if (this.formData.telefono === '') {
+              data.telefono = undefined;
+            }
+          if (!expnumber.test(this.formData.telefono) && this.formData.telefono != undefined && this.formData.telefono != '') {
+            console.log("michi",this.formData.telefono);
+            
             this.mensaje = " Ingresa un telefono valido ";
             this.showMensaje = true;
           } else {
@@ -155,23 +144,27 @@ export default {
 </script>
 
 <style scoped>
-h1{
+h1 {
   text-align: center;
   letter-spacing: 2px;
   font-size: 20px;
 }
-h2{
+
+h2 {
   margin: 0.5em;
   text-align: center;
   font-size: 17px;
-  color:rgb(255, 217, 134);
+  color: rgb(255, 217, 134);
 }
-h1:hover,h2:hover{
+
+h1:hover,
+h2:hover {
   filter: brightness(1.12);
   transform: scale(1.1155);
   transition: 400ms;
   animation: all;
 }
+
 #banner-principal {
   font-weight: 700;
   padding-bottom: 10px;
@@ -183,10 +176,10 @@ h1:hover,h2:hover{
   filter: sepia(1.65);
   filter: opacity(82%);
   opacity: 0.9;
-  background-size:cover;
+  background-size: cover;
   background-attachment: fixed;
   animation: transiciones 522020ms ease-in-out infinite forwards;
-  
+
 }
 
 @keyframes transiciones {
@@ -194,60 +187,66 @@ h1:hover,h2:hover{
     background: url(../assets/img/carousel_principal/ACTECK.webp);
     background-size: 100%;
     transition: 42s;
-  background-position: center;
-  background-repeat: no-repeat;
-    
+    background-position: center;
+    background-repeat: no-repeat;
+
   }
+
   15% {
     background: url(../assets/img/carousel_principal/CALVIN_KLEIN.webp);
     background-size: 100%;
     transition: 42s;
-  background-position: center;
-  background-repeat: no-repeat;
+    background-position: center;
+    background-repeat: no-repeat;
   }
+
   30% {
     background: url(../assets/img/carousel_principal/CITIZEN.webp);
     background-size: 100%;
-     background-position: center;
-  background-repeat: no-repeat;
-  transition: 42s;
+    background-position: center;
+    background-repeat: no-repeat;
+    transition: 42s;
   }
+
   45% {
     background: url(../assets/img/carousel_principal/ORTIZ_ESPINOZA.webp);
     background-size: 100%;
     transition: 42s;
-  background-position: center;
-  background-repeat: no-repeat;
+    background-position: center;
+    background-repeat: no-repeat;
   }
 
   60% {
     background: url(../assets/img/carousel_principal/SHEAFFER.webp);
     background-size: 100%;
     transition: 42s;
-  background-position: center;
-  background-repeat: no-repeat;
+    background-position: center;
+    background-repeat: no-repeat;
   }
-   75% {
+
+  75% {
     background: url(../assets/img/carousel_principal/SWISSBRAND.webp);
     background-size: 100%;
-  background-position: center;
-  background-repeat: no-repeat;
-  transition: 42s;
+    background-position: center;
+    background-repeat: no-repeat;
+    transition: 42s;
   }
-   85% {
+
+  85% {
     background: url(../assets/img/carousel_principal/UNO_DE_50.webp);
     background-size: 100%;
-  background-position: center;
-  background-repeat: no-repeat;
-  transition: 42s;
+    background-position: center;
+    background-repeat: no-repeat;
+    transition: 42s;
   }
-   100% {
+
+  100% {
     background: url(../assets/img/carousel_principal/VICTORINOX.webp);
     background-size: 100%;
     background-color: black;
-  background-position: center;
-  background-repeat: no-repeat;
-  transition: 42s;
+    background-position: center;
+    background-repeat: no-repeat;
+    transition: 42s;
   }
 }
 
@@ -261,10 +260,12 @@ h1:hover,h2:hover{
     rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
     rgba(204, 29, 29, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
 }
-#primario:hover{
+
+#primario:hover {
   background: rgba(0, 0, 0, 0.979);
   transition: all 300ms;
 }
+
 #registro {
   display: flex;
   align-items: center;
@@ -276,16 +277,19 @@ h1:hover,h2:hover{
 .formulario {
   width: 80%;
 }
+
 #form {
   display: flex;
   flex-flow: column;
   justify-content: center;
   margin-top: 70px;
 }
+
 form label,
 form h1 {
   color: white;
 }
+
 .message__container {
   margin-top: 10px;
   display: flex;
@@ -295,10 +299,12 @@ form h1 {
   border-radius: 10px;
   background: linear-gradient(to bottom, #f2f2f21f, #11111198);
 }
+
 #mensaje-validacion,
 hr {
   color: white;
 }
+
 input::placeholder {
   color: rgb(255, 255, 255);
 }
@@ -316,19 +322,23 @@ form select {
   color: rgb(255, 255, 255);
   font-size: 14px;
 }
+
 form select option {
   padding: 1.5em;
 }
+
 label,
 input,
 button,
 select {
   margin: 10px;
 }
-form label i{
+
+form label i {
   color: white;
   margin: 0.4em;
 }
+
 form button {
   width: 100px;
   margin: 0 auto;
@@ -340,17 +350,21 @@ form button {
   width: 50vw;
   padding: 0 0.5em;
 }
+
 .message__container i {
   color: yellow;
 }
+
 form {
   top: 50%;
   left: 50%;
 }
+
 @media (max-width: 739px) {
-  #banner_principal{
+  #banner_principal {
     height: auto;
   }
+
   #primario {
     width: 100%;
     height: auto;
